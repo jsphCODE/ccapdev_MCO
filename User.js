26efdf99ff3300a6bce6731b2bbdcf5794473bdc
Joshua Habos <147166254+jsphCODE@users.noreply.github.com>
2025-11-15 20:57:59 +0800

    Updated User model schema

---
+++ User.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    //Not required since some people may not have middle names
+    middleName: {
+        type: String,
+        trim: true
+    },
+
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+
+    //Phone number is not required to create a user account
+    phone: {
+        type: String,
+        //match: '^(09|\+639)\d{9}$',
+        trim: true,
+        unique: true
+    },
+    
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+
+    role: {
+        type: String,
+        enum: ['User','Admin'],
+        default: 'User'
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', UserSchema);
