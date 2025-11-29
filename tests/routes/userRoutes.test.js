b19d55da028d774af32c6d3ffd05a6106c72be3f
Joshua Habos <147166254+jsphCODE@users.noreply.github.com>
2025-11-29 13:38:37 +0800

    Update userRoutes.test.js

--- tests/routes/userRoutes.test.js
+++ tests/routes/userRoutes.test.js
@@ -1,28 +1,73 @@
+const mongoose = require("mongoose");
 const request = require("supertest");
 const app = require("../app");
 require("../setup");
 const User = require("../../models/User");
+const bcrypt = require('bcrypt');
 
 describe("User Routes", () => {
 
+    test("GET render user registration page", async () => {
+        const res = await request(app).get("/register");
+        expect(res.statusCode).toBe(200);
+    });
+    
     test("POST /register creates a user", async () => {
-        const res = await request(app)
-            .post("/register")
-            .send({ username: "test", email: "test@mail.com", password: "1234" });
+        let formData = {firstName: 'Testing', middleName: 'The', lastName: 'Test',
+                        email: 'test@mail.com', username: 'testingTHEtest',
+                        password: 'Testing!@#123'}
 
-        const user = await User.findOne({ username: "test" });
+        const res = await request(app).post("/register").send(formData);
+        expect(res.statusCode).toBe(200);
+
+        const session = mongoose.connection.startSession();
+        expect(session).toBeDefined();
+
+        const user = new User(formData);
         expect(user).not.toBeNull();
+
+        await session.endSession(); // End the session after testing
+    });
+
+    test("GET render user login page", async () => {
+        const res = await request(app).get("/login");
+        expect(res.statusCode).toBe(200);
     });
 
     test("POST /login logs in a user", async () => {
         // prepare test user
-        await User.create({ username: "test2", email: "test2@mail.com", password: "1234" });
+        const test = await User.create({ username: "test2", email: "test2@mail.com", password: "1234" });
 
-        const res = await request(app)
-            .post("/login")
-            .send({ username: "test2", email: "test2@mail.com", password: "1234" });
+        const userCheck = await User.findOne({ username: test.username }).lean();
 
+        //User not found
+        if (!userCheck) {
+            expect(userCheck).toBeNull();
+            expect(res.statusCode).toBe(404);
+            next()
+        }
+        
+        //Email is incorrect
+        if (userCheck.email !== test.email) {
+            expect(res.statusCode).toBe(404);
+            next()
+        }
+        
+        let passwordCheck = bcrypt.compare(test.password, userCheck.password);
+        
+        //Password does not match
+        if (!passwordCheck) {
+            expect(res.statusCode).toBe(404);
+            next()
+        }
+        
+        const session = mongoose.connection.startSession();
+        expect(session).toBeDefined();
+        
+        const res = await request(app).post("/login").send({ username: "test2", email: "test2@mail.com", password: "1234" });
         expect(res.statusCode).toBe(200);
+
+        await session.endSession(); // End the session after testing
     });
 
 });
